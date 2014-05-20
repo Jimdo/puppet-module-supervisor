e01c1afbf030418572fce656cebd26dff48b0e29
Jan Brauer <jan@jimdo.com>
2014-05-20 12:57:45 +0200

    Sleep after update

--- manifests/update.pp
+++ manifests/update.pp
@@ -1,6 +1,6 @@
 class supervisor::update {
   exec { 'supervisor::update':
-    command     => '/usr/bin/supervisorctl update',
+    command     => '/usr/bin/supervisorctl update || sleep 5',
     logoutput   => on_failure,
     refreshonly => true,
     require     => Service[$supervisor::params::system_service],
